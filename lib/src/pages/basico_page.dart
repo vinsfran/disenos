16a1730e4db0182a3ab8e4a19a8a002806a25be5
Vicente Insfran <vinsfra@gmail.com>
2021-03-04 20:20:33 -0300

    131. Diseño básico - parte 1

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -1,12 +1,48 @@
 import 'package:flutter/material.dart';
 
 class BasicoPage extends StatelessWidget {
+  final estiloTitulo = TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
+  final estiloSubTitulo = TextStyle(fontSize: 18.0, color: Colors.grey);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Basico Page'),
-      ),
+      body: Column(children: <Widget>[
+        Image(
+          image: NetworkImage(
+              'https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Lago con un puente',
+                      style: estiloTitulo,
+                    ),
+                    SizedBox(
+                      height: 7.0,
+                    ),
+                    Text(
+                      'Un lago en Alemania',
+                      style: estiloSubTitulo,
+                    ),
+                  ],
+                ),
+              ),
+              Icon(Icons.star, color: Colors.red, size: 30.0),
+              Text(
+                '41',
+                style: TextStyle(fontSize: 20.0),
+              )
+            ],
+          ),
+        )
+      ]),
     );
   }
 }
