b3a0dddf10cc4d1ea49a8b9fad8b68453c608957
Vicente Insfran <vinsfra@gmail.com>
2021-03-04 20:52:47 -0300

    132. Diseño básico - parte 2

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -8,41 +8,98 @@ class BasicoPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(children: <Widget>[
-        Image(
-          image: NetworkImage(
-              'https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
-        ),
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-          child: Row(
-            children: <Widget>[
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'Lago con un puente',
-                      style: estiloTitulo,
-                    ),
-                    SizedBox(
-                      height: 7.0,
-                    ),
-                    Text(
-                      'Un lago en Alemania',
-                      style: estiloSubTitulo,
-                    ),
-                  ],
+        _crearImagen(),
+        _crearTitulo(),
+        _crearAcciones(),
+        _crearTexto(),
+        _crearTexto(),
+        _crearTexto(),
+        _crearTexto(),
+        _crearTexto(),
+      ]),
+    );
+  }
+
+  Widget _crearImagen() {
+    return Image(
+      image: NetworkImage(
+          'https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+    );
+  }
+
+  Widget _crearTitulo() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Lago con un puente',
+                  style: estiloTitulo,
                 ),
-              ),
-              Icon(Icons.star, color: Colors.red, size: 30.0),
-              Text(
-                '41',
-                style: TextStyle(fontSize: 20.0),
-              )
-            ],
+                SizedBox(
+                  height: 7.0,
+                ),
+                Text(
+                  'Un lago en Alemania',
+                  style: estiloSubTitulo,
+                ),
+              ],
+            ),
+          ),
+          Icon(Icons.star, color: Colors.red, size: 30.0),
+          Text(
+            '41',
+            style: TextStyle(fontSize: 20.0),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _crearAcciones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        _accion(Icons.call, 'Call'),
+        _accion(Icons.near_me, 'Route'),
+        _accion(Icons.share, 'Share'),
+      ],
+    );
+  }
+
+  Widget _accion(IconData icon, String texto) {
+    return Column(
+      children: <Widget>[
+        Icon(
+          icon,
+          color: Colors.blue,
+          size: 40.0,
+        ),
+        SizedBox(
+          height: 5.0,
+        ),
+        Text(
+          texto,
+          style: TextStyle(
+            fontSize: 15.0,
+            color: Colors.blue,
           ),
         )
-      ]),
+      ],
+    );
+  }
+
+  Widget _crearTexto() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+      child: Text(
+        'Duis eiusmod dolore nisi quis Lorem aliquip nostrud ex labore enim Lorem ad aliquip ex. Magna ut aute nisi qui ea cillum eu nulla adipisicing eu sit amet eiusmod. Aliqua et adipisicing incididunt proident cupidatat dolore pariatur consequat aliqua magna eiusmod dolor. Tempor incididunt ad Lorem fugiat irure mollit aute sint non ea duis. Laborum voluptate eiusmod enim officia. Minim magna mollit minim minim commodo incididunt exercitation consequat aute aliquip elit excepteur excepteur Lorem.',
+        textAlign: TextAlign.justify,
+      ),
     );
   }
 }
