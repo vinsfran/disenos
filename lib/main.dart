7f1a83eee2d67adc5689ce98afbf6b45e011696b
Vicente Insfran <vicente.insfran@asuncion.gov.py>
2021-03-04 15:17:09 -0300

    130. Inicio de proyecto - Diseños

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'package:disenos/src/pages/basico_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Diseños',
+      initialRoute: 'basico',
+      routes: {
+        'basico': (BuildContext context) => BasicoPage(),
+      },
+    );
+  }
+}
