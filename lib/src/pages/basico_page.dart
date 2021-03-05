c6583d8aff1abc0ae9773494b9a46b768eed21b8
Vicente Insfran <vinsfra@gmail.com>
2021-03-04 22:02:19 -0300

    133. Diseño básico - parte 3

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -7,55 +7,66 @@ class BasicoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: <Widget>[
-        _crearImagen(),
-        _crearTitulo(),
-        _crearAcciones(),
-        _crearTexto(),
-        _crearTexto(),
-        _crearTexto(),
-        _crearTexto(),
-        _crearTexto(),
-      ]),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            _crearImagen(),
+            _crearTitulo(),
+            _crearAcciones(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+          ],
+        ),
+      ),
     );
   }
 
   Widget _crearImagen() {
-    return Image(
-      image: NetworkImage(
-          'https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+    return Container(
+      width: double.infinity,
+      child: Image(
+        image: NetworkImage(
+            'https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+        height: 200.0,
+        fit: BoxFit.cover,
+      ),
     );
   }
 
   Widget _crearTitulo() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  'Lago con un puente',
-                  style: estiloTitulo,
-                ),
-                SizedBox(
-                  height: 7.0,
-                ),
-                Text(
-                  'Un lago en Alemania',
-                  style: estiloSubTitulo,
-                ),
-              ],
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Lago con un puente',
+                    style: estiloTitulo,
+                  ),
+                  SizedBox(
+                    height: 7.0,
+                  ),
+                  Text(
+                    'Un lago en Alemania',
+                    style: estiloSubTitulo,
+                  ),
+                ],
+              ),
             ),
-          ),
-          Icon(Icons.star, color: Colors.red, size: 30.0),
-          Text(
-            '41',
-            style: TextStyle(fontSize: 20.0),
-          )
-        ],
+            Icon(Icons.star, color: Colors.red, size: 30.0),
+            Text(
+              '41',
+              style: TextStyle(fontSize: 20.0),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -94,11 +105,13 @@ class BasicoPage extends StatelessWidget {
   }
 
   Widget _crearTexto() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
-      child: Text(
-        'Duis eiusmod dolore nisi quis Lorem aliquip nostrud ex labore enim Lorem ad aliquip ex. Magna ut aute nisi qui ea cillum eu nulla adipisicing eu sit amet eiusmod. Aliqua et adipisicing incididunt proident cupidatat dolore pariatur consequat aliqua magna eiusmod dolor. Tempor incididunt ad Lorem fugiat irure mollit aute sint non ea duis. Laborum voluptate eiusmod enim officia. Minim magna mollit minim minim commodo incididunt exercitation consequat aute aliquip elit excepteur excepteur Lorem.',
-        textAlign: TextAlign.justify,
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 40.0),
+        child: Text(
+          'Duis eiusmod dolore nisi quis Lorem aliquip nostrud ex labore enim Lorem ad aliquip ex. Magna ut aute nisi qui ea cillum eu nulla adipisicing eu sit amet eiusmod. Aliqua et adipisicing incididunt proident cupidatat dolore pariatur consequat aliqua magna eiusmod dolor. Tempor incididunt ad Lorem fugiat irure mollit aute sint non ea duis. Laborum voluptate eiusmod enim officia. Minim magna mollit minim minim commodo incididunt exercitation consequat aute aliquip elit excepteur excepteur Lorem.',
+          textAlign: TextAlign.justify,
+        ),
       ),
     );
   }
