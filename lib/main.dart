0df0bafde28a7c563625d3893735fc675440945e
Vicente Insfran <vinsfra@gmail.com>
2021-03-05 21:53:05 -0300

    134. Diseño con scroll - parte 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:disenos/src/pages/basico_page.dart';
+import 'package:disenos/src/pages/scroll_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,9 +11,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
-      initialRoute: 'basico',
+      initialRoute: 'scroll',
       routes: {
         'basico': (BuildContext context) => BasicoPage(),
+        'scroll': (BuildContext context) => ScrollPage(),
       },
     );
   }
