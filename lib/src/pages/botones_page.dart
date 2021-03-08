aef6228b6e3943d2dcf7380b592efb80992a83d5
Vicente Insfran <vinsfra@gmail.com>
2021-03-08 20:18:37 -0300

    140. Diseño compuesto - Table y TableRow

--- lib/src/pages/botones_page.dart
+++ lib/src/pages/botones_page.dart
@@ -13,6 +13,7 @@ class BotonesPage extends StatelessWidget {
             child: Column(
               children: <Widget>[
                 _titulos(),
+                _botonesRedondeados(),
               ],
             ),
           ),
@@ -118,6 +119,72 @@ class BotonesPage extends StatelessWidget {
               size: 30.0,
             ),
             label: '',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _botonesRedondeados() {
+    return Table(
+      children: [
+        TableRow(
+          children: [
+            _crearBotonRedondeado(),
+            _crearBotonRedondeado(),
+          ],
+        ),
+        TableRow(
+          children: [
+            _crearBotonRedondeado(),
+            _crearBotonRedondeado(),
+          ],
+        ),
+        TableRow(
+          children: [
+            _crearBotonRedondeado(),
+            _crearBotonRedondeado(),
+          ],
+        ),
+        TableRow(
+          children: [
+            _crearBotonRedondeado(),
+            _crearBotonRedondeado(),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _crearBotonRedondeado() {
+    return Container(
+      height: 180.0,
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(62, 66, 107, 0.7),
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          SizedBox(
+            height: 5.0,
+          ),
+          CircleAvatar(
+            backgroundColor: Colors.pinkAccent,
+            radius: 35.0,
+            child: Icon(
+              Icons.swap_calls,
+              color: Colors.white,
+              size: 30.0,
+            ),
+          ),
+          Text(
+            'Cosa',
+            style: TextStyle(color: Colors.pinkAccent),
+          ),
+          SizedBox(
+            height: 5.0,
           ),
         ],
       ),
